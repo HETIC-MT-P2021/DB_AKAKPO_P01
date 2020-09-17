410cc8d7bf68483c94edb42c62da1992f9808552
Jean-Jacques Akakpo <akakpo.jeanjacques@gmail.com>
2020-09-17 16:33:25 +0200

    🚀 Customer request and sql query ready

--- controllers/customer.go
+++ controllers/customer.go
@@ -1,15 +1,19 @@
 package controllers
 
 import (
+	"akakpo/db/models"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-// ReadAllCustomers Reads all customers from database
-func ReadAllCustomers(c *gin.Context) {
-	c.JSON(http.StatusCreated, gin.H{
+// GetCustomer Reads all customers from database
+func GetCustomer(c *gin.Context) {
+	id := c.Param("id")
+	response, _ := models.ReadCustomer(id)
+
+	c.JSON(http.StatusOK, gin.H{
 		"success": true,
-		"content": "",
+		"content": response,
 	})
 }
--- models/customer.go
+++ models/customer.go
@@ -1,16 +1,50 @@
 package models
 
-import (
-	"net/http"
+import "akakpo/db/types"
 
-	"github.com/gin-gonic/gin"
-)
+// ReadCustomer reads all the customers
+func ReadCustomer(id string) ([]types.CustomerResult, error) {
+	sql := `
+		SELECT
+			(SELECT CONCAT(E.lastName, ' ', E.firstName) FROM employees E WHERE E.employeeNumber = C.salesRepEmployeeNumber) AS salesRespEmp,
+			O.orderNumber,
+			COUNT(OD.productCode) AS nbOrderedProducts,
+			SUM(OD.quantityOrdered * OD.priceEach) AS totalPrice
+		FROM orders O
+			INNER JOIN orderdetails OD ON O.orderNumber = OD.orderNumber
+			INNER JOIN customers C ON O.customerNumber = C.customerNumber
+		WHERE O.customerNumber = 112
+		GROUP BY O.orderNumber
+	`
 
-// ReadAllCustomers reads all the customers
-func ReadAllCustomers(c *gin.Context) {
-	c.JSON(http.StatusCreated, gin.H{
-		"success": true,
-		"message": "Reads all customers",
-		"content": nil,
-	})
+	var response []types.CustomerResult
+	var err error
+
+	rows, _ := Database.Query(sql)
+	for rows.Next() {
+		item := types.CustomerResult{}
+		err = rows.Scan(
+			&item.CustomerNumber,
+			&item.CustomerName,
+			&item.ContactLastName,
+			&item.ContactFirstName,
+			&item.Phone,
+			&item.AddressLine1,
+			&item.AddressLine2,
+			&item.City,
+			&item.State,
+			&item.PostalCode,
+			&item.Country,
+			&item.SalesRepEmployeeNumber,
+			&item.CreditLimit,
+			&item.SalesRespEmp,
+			&item.OrderNumber,
+			&item.NbOrderedProducts,
+			&item.TotalPrice,
+		)
+
+		response = append(response, item)
+	}
+
+	return response, err
 }
