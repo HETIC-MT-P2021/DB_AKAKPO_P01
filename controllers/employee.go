6cf4af55bbac898f116beb3154230488903c21b5
Jean-Jacques Akakpo <akakpo.jeanjacques@gmail.com>
2020-09-17 16:32:36 +0200

    🚀 Employee request and sql query ready

--- controllers/employee.go
+++ controllers/employee.go
@@ -1,15 +1,19 @@
 package controllers
 
 import (
+	"akakpo/db/models"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-// ReadAllEmployees Reads all employees from database
-func ReadAllEmployees(c *gin.Context) {
-	c.JSON(http.StatusCreated, gin.H{
+// GetEmployee Reads all employees from database
+func GetEmployee(c *gin.Context) {
+	id := c.Param("id")
+	response, _ := models.ReadEmployee(id)
+
+	c.JSON(http.StatusOK, gin.H{
 		"success": true,
-		"content": "",
+		"content": response,
 	})
 }
--- models/employee.go
+++ models/employee.go
@@ -1,16 +1,33 @@
 package models
 
 import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
+	"akakpo/db/types"
 )
 
-// ReadAllEmployees reads all the employees
-func ReadAllEmployees(c *gin.Context) {
-	c.JSON(http.StatusCreated, gin.H{
-		"success": true,
-		"message": "Reads all the employees",
-		"content": nil,
-	})
+// ReadEmployee reads all the employees
+func ReadEmployee(id string) (types.EmployeeResult, error) {
+	sql := `
+		SELECT
+			E.employeeNumber,
+			E.lastName,
+			E.firstName,
+			E.extension,
+			E.email,
+			E.jobTitle,
+			(SELECT CONCAT(firstName, ' ', lastName) AS fullName FROM employees WHERE employeeNumber = E.reportsTo) AS reportsTo,
+			O.city AS officeCity,
+			O.phone AS officePhone,
+			O.addressLine1 AS officeAddressLine1,
+			O.addressLine2 AS officeAddressLine2
+		FROM employees E
+		INNER JOIN offices O ON E.officeCode = O.officeCode
+		WHERE E.employeeNumber = 1056
+	`
+
+	var response types.EmployeeResult
+
+	row := Database.QueryRow(sql)
+	err := row.Scan(&response.EmployeeNumber, &response.LastName, &response.FirstName, &response.Extension, &response.Email, &response.JobTitle, &response.ReportsTo, &response.OfficeCity, &response.OfficePhone, &response.OfficeAddressLine1, &response.OfficeAddressLine2)
+
+	return response, err
 }
