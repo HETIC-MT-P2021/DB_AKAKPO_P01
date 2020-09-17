304f471170fc7e34088dc4bec909b32e5381057b
Jean-Jacques Akakpo <akakpo.jeanjacques@gmail.com>
2020-09-17 16:32:59 +0200

    🚀 Order request and sql query ready

--- controllers/order.go
+++ controllers/order.go
@@ -1,15 +1,19 @@
 package controllers
 
 import (
+	"akakpo/db/models"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-// ReadAllOrders Reads all orders from database
-func ReadAllOrders(c *gin.Context) {
-	c.JSON(http.StatusCreated, gin.H{
+// GetOrder Reads all orders from database
+func GetOrder(c *gin.Context) {
+	id := c.Param("id")
+	response, _ := models.ReadOrder(id)
+
+	c.JSON(http.StatusOK, gin.H{
 		"success": true,
-		"content": "",
+		"content": response,
 	})
 }
--- models/order.go
+++ models/order.go
@@ -1,16 +1,31 @@
 package models
 
-import (
-	"net/http"
+import "akakpo/db/types"
 
-	"github.com/gin-gonic/gin"
-)
+// ReadOrder reads all the orders
+func ReadOrder(id string) ([]types.OrderResult, error) {
+	sql := `
+		SELECT
+			(SELECT P.productName FROM products P WHERE P.productCode = OD.productCode) AS productName,
+			OD.quantityOrdered,
+			OD.priceEach,
+			OD.orderLineNumber
+		FROM orderdetails OD INNER JOIN orders O ON OD.orderNumber = O.orderNumber
+		WHERE O.orderNumber = 10115
+		GROUP BY OD.productCode
+	`
 
-// ReadAllOrders reads all the orders
-func ReadAllOrders(c *gin.Context) {
-	c.JSON(http.StatusCreated, gin.H{
-		"success": true,
-		"message": "Reads all the orders",
-		"content": nil,
-	})
+	var response []types.OrderResult
+	var err error
+
+	rows, _ := Database.Query(sql)
+
+	for rows.Next() {
+		item := types.OrderResult{}
+		err = rows.Scan(&item.ProductName, &item.QuantityOrdered, &item.PriceEach, &item.OrderLineNumber)
+
+		response = append(response, item)
+	}
+
+	return response, err
 }
